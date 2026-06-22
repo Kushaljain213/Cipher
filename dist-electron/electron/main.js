aa2fb1d5f12dc88713dbcee047176730df98edf5
Kushaljain213 <kushaljain2062@gmail.com>
2026-06-22 22:11:16 +0530

    Add files via upload

---
+++ dist-electron/electron/main.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow, Menu } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.commandLine.appendSwitch("force-device-scale-factor", "1");
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+    if (process.env.VITE_DEV_SERVER_URL) {
+        win.loadURL(process.env.VITE_DEV_SERVER_URL);
+    }
+    else {
+        win.loadFile(path.join(__dirname, "../../dist/index.html"));
+    }
+    Menu.setApplicationMenu(null);
+}
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
